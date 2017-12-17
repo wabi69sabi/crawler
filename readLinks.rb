0be94be56b67ab63c697ebe61d85227a10454788
Pasquale Kress <wabisabisf@gmail.com>
2017-12-17 23:06:20 +0100

    small clean ups

--- readLinks.rb
+++ readLinks.rb
@@ -2,7 +2,13 @@ require 'gdbm'
 
 gdbm = GDBM.new("links.db")
 
+cleaned = []
+
 gdbm.each_pair do |index, link|
-  puts "#{index} --> #{link}"
+  # puts "#{index} --> #{link}"
+  cleaned << "#{index} --> #{link}"
 end
+
 gdbm.close
+puts cleaned.sort
+
--- scraper.rb
+++ scraper.rb
@@ -14,21 +14,18 @@ links_a = doc.css("a")
 puts "<link> : #{links.length}"
 puts "<a> : #{links_a.length}"
 
-gdbm = GDBM.new("links.db")
+$gdbm = GDBM.new("links.db")
 
-links.each_with_index do |link, index|
-  puts link['href']
-  unless link['href'].nil?
-    gdbm[index.to_s] = link['href']
+def mine_links(array)
+  array.each_with_index do |link, index|
+    puts "#{index + 1}. #{link['href']}"
+    unless link['href'].nil?
+      $gdbm[index.to_s] = link['href']
+    end
   end
 end
 
-links_a.each_with_index do |link, index|
-  puts "#{index + 1}. #{link['href']}"
-  unless link['href'].nil?
-    gdbm[index.to_s] = link['href']
-  end
-end
-
-gdbm.close
+mine_links(links)
+mine_links(links_a)
+$gdbm.close
 # https://www.icicletech.com/blog/web-scraping-with-ruby-using-mechanize-and-nokogiri-gems
